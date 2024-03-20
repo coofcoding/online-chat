a145c2f4c2fc21986a760206cd8b9afdf7f862df
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-03-20 17:57:59 -0500

    website font changed by Montserrat

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from 'react-router-dom';
-import '@fontsource/roboto/100.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import '@fontsource/roboto/900.css';
+// Supports weights 100-900
+import '@fontsource-variable/montserrat';
 import '@fontsource/pacifico';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
