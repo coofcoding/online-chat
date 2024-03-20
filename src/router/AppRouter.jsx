40ba36247fcc4689b6893d663a7d7a8e866ec8e1
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-03-20 17:57:18 -0500

    home page configured

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,28 +1,26 @@
 import { useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import { LoginPage, RegisterPage } from "../auth/pages";
+import { LoginPage, RegisterPage, HomePage } from "../auth/pages";
 import { ContentPage } from "../home/pages/ContentPage";
 
 export const AppRouter = () => {
-  
-    const [status, setStatus] = useState('not-authenticated');
-  
+    const [status, setStatus] = useState("not-authenticated");
+
     return (
         <Routes>
-            {
-                status === 'not-authenticated' 
-                ? 
+            {status === "not-authenticated" ? (
                 <>
-                    <Route path="/auth/*" element={ <LoginPage/> }/>
-                    <Route path="/auth/register" element={ <RegisterPage/> }/>
-                    <Route path="/*" element={ <Navigate to="auth/login" /> }/>
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/auth/*" element={<LoginPage />} />
+                    <Route path="/auth/register" element={<RegisterPage />} />
+                    <Route path="/*" element={<Navigate to="home" />} />
                 </>
-                :
+            ) : (
                 <>
-                    <Route path="/" element={<ContentPage/>} />
-                    <Route path="/*" element={ <Navigate to="/"/> } />
+                    <Route path="/" element={<ContentPage />} />
+                    <Route path="/*" element={<Navigate to="/" />} />
                 </>
-            }
+            )}
         </Routes>
     );
 };
